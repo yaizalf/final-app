03cc99836528af6e438a271a435d134eaa29290b
yaizalf <lfyaiza@gmail.com>
2020-09-22 02:15:40 +0200

    Show date & time

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// Show Today
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = weekDays[now.getDay()];
+
+let todayDate = document.querySelector("#today");
+todayDate.innerHTML = `${day}, ${hours}:${minutes}`;
