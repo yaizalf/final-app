fb786a03a96b28d26a5832a955f43c4a3b064236
yaizalf <lfyaiza@gmail.com>
2020-09-22 16:44:33 +0200

    Minutes and hours problem fixed

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@
 let now = new Date();
 let hours = now.getHours();
 let minutes = now.getMinutes();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 let weekDays = [
   "Sunday",
